9fc2c9d313f0ff6d47281d8507a1ca3a4ba1ca1c
zaoqi <zaomir@outlook.com>
2019-01-04 10:39:53 +0000

    .

--- rc.lua
+++ rc.lua
@@ -9,7 +9,6 @@ local hotkeys_popup = require("awful.hotkeys_popup").widget
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-
 local freedesktop = require("freedesktop")
 
 -- Check if awesome encountered an error during startup and fell back to
