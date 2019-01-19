042cd3c9058b4506ca3169009f4082d884fa633d
zaoqi <zaomir@outlook.com>
2019-01-19 02:50:54 +0000

    .

--- rc.lua
+++ rc.lua
@@ -45,19 +45,6 @@ awful.layout.layouts = {
     awful.layout.suit.max,
 }
 
-local function client_menu_toggle_fn()
-    local instance = nil
-
-    return function ()
-        if instance and instance.wibox.visible then
-            instance:hide()
-            instance = nil
-        else
-            instance = awful.menu.clients{ theme = { width = 250 } }
-        end
-    end
-end
-
 local mykeyboardheight = 100
 local mykeyboardbar = awful.wibar{ position = "bottom", visible = false, height = mykeyboardheight }
 local mykeyboard = "xvkbd"
